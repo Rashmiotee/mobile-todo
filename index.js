cfc697fc68e635ac1aff4a29134a982ba3b8525f
Rashmi_otee <rashmiotee55@gmail.com>
2024-12-24 09:25:03 +0530

    error

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ app.use("/api/employee", require("./routes/employee.route"))
 app.use("*", (req, res) => {
     res.status(404).json({ message: "resource not found" })
 })
+app.use((err, req, res, next) => {
+    if (err) {
+        console.log(err)
+        return res.status(500).json({ message: "something went wrong" })
 
+    }
+})
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.once("open", () => {
     console.log("mongo connect")
